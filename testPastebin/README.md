72011e7e402135312ae9a71c9d319d70d0a45dad
SharifCoding <ocarinaoftime81@gmail.com>
2018-12-20 20:20:14 +0000

    three specs added to pastebin test

--- testPastebin/README.md
+++ testPastebin/README.md
@@ -16,6 +16,26 @@ webdriver-manager start
 protractor conf.js
 ```
 
+### Jasmine Output
+```js
+[20:16:26] I/launcher - Running 1 instances of WebDriver
+[20:16:26] I/hosted - Using the selenium server at http://localhost:4444/wd/hub
+Started
+Jasmine started
+.
+  Protractor Demo
+    ✓ should display the name of the application
+.    ✓ should click the create Paste button
+.    ✓ should accept and save input values
+
+3 specs, 0 failures
+Finished in 8.562 seconds
+
+Executed 3 of 3 specs SUCCESS in 9 secs.
+[20:16:38] I/launcher - 0 instance(s) of WebDriver still running
+[20:16:38] I/launcher - chrome #01 passed
+```
+
 #### Further Reading
 - [E2E tests for a Pastebin app](https://code.tutsplus.com/tutorials/getting-started-with-end-to-end-testing-in-angular-using-protractor--cms-29318) - Getting Started With End-to-End Testing in Angular Using Protractor.
 
--- testPastebin/pastebin-spec.js
+++ testPastebin/pastebin-spec.js
@@ -1,21 +1,35 @@
 describe('Protractor Demo', () => {
 
   beforeEach(() => {
-    // The code here will get executed before each it block is called  
     browser.get('http://laserpants.github.io/pastebin-angular/#/');
   });
  
   it('should display the name of the application',() => {
-   /* Expectations accept parameters that will be matched with the real value
-   using Jasmine's matcher functions. eg. toEqual(),toContain(), toBe(), toBeTruthy() etc. */
-   expect(browser.getTitle()).toEqual('Insignificant Pastebin App');
-   expect(element(by.className('navbar-brand')).getText()).toContain('Insignificant Pastebin App');
+    browser.sleep(1000);
+    expect(browser.getTitle()).toEqual('Insignificant Pastebin App');
+    expect(element(by.className('navbar-brand')).getText()).toContain('Insignificant Pastebin App');
   });
    
   it('should click the create Paste button',() => {
-    expect(element(by.name('form')).isPresent()).toBeFalsy('The modal window shouldn\'t appear right now');
+    browser.sleep(1000);
+    expect(element(by.name('form')).isPresent()).toBeFalsy('The form shouldn\'t appear right now');
     element(by.partialLinkText('Paste code')).click();
-    expect(element(by.name('form')).isPresent()).toBeTruthy('The modal window should appear now');    
+    browser.sleep(1000);
+    expect(element(by.name('form')).isPresent()).toBeTruthy('The form should appear now');    
   });
 
+  it('should accept and save input values', () => {
+    element(by.partialLinkText('Paste code')).click();
+
+    // send input values to the form using sendKeys
+    element(by.name('title')).sendKeys('Hello world in Javascript');
+    element(by.cssContainingText('option', 'Javascript')).click();
+    element(by.name('body')).sendKeys('console.log(\"Hello, World!\");');
+    element(by.css('input[type="submit"]')).click();
+    browser.sleep(2000);
+
+    // expect the table to contain the new paste
+    const lastRow = element.all(by.tagName('tr')).last();
+    expect(lastRow.getText()).toContain("Hello world in Javascript");
+});
 });
